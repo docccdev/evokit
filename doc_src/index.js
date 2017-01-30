ddffa51069c5899c6920be1500d06ea555da289c
e.marchenko <e.marchenko@smartweb.com.ua>
2017-01-30 14:26:20 +0200

    add template for doc

---
+++ doc_src/index.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "root_dir":       __dirname + "/__root",
+    "base":             __dirname + "/tmpl/base.html",
+    "block": {
+        "code":         __dirname + "/tmpl/block/code.html",
+        "codespan":     __dirname + "/tmpl/block/codespan.html",
+        "hr":           __dirname + "/tmpl/block/hr.html",
+        "heading":      __dirname + "/tmpl/block/heading.html",
+        "paragraph":    __dirname + "/tmpl/block/paragraph.html",
+        "table":        __dirname + "/tmpl/block/table.html",
+        "tablerow":     __dirname + "/tmpl/block/tablerow.html",
+        "tablecell":    __dirname + "/tmpl/block/tablecell.html"
+    }
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,57 +1,53 @@
 var gulp = require('gulp');
+var gulpCopy = require('gulp-contrib-copy');
 var stylus = require('gulp-stylus');
 var autoprefixer = require('autoprefixer-stylus');
 var gulpMdDocs = require('gulp-md-docs');
+var docTemplate = require('./doc_src/index.js');
 
-gulp.task('desktop', function () {
+gulp.task('compile_doc_css', function () {
+    return gulp.src('./doc_src/styl/doc.styl')
+        .pipe(stylus({
+            compress: true,
+            use: [autoprefixer('last 5 versions')]
+        }))
+        .pipe(gulp.dest('./doc_src/__root/css'));
+});
+
+gulp.task('copy_template_root_dir', function() {
+    return gulp.src(docTemplate.root_dir + '/**/*')
+        .pipe(gulpCopy())
+        .pipe(gulp.dest('doc_dist'));
+});
+
+gulp.task('compile_desktop_uikit', function () {
     return gulp.src('./src/styl/bundles/desktop.styl')
         .pipe(stylus({
             compress: true,
             use: [autoprefixer('last 5 versions')]
         }))
-        .pipe(gulp.dest('./documentation/css'));
+        .pipe(gulp.dest('./doc_src/__root/css'));
 });
 
-gulp.task('portable', function () {
+gulp.task('compile_portable_uikit', function () {
     return gulp.src('./src/styl/bundles/portable.styl')
         .pipe(stylus({
             compress: true,
             use: [autoprefixer('last 5 versions')]
         }))
-        .pipe(gulp.dest('./documentation/css'));
+        .pipe(gulp.dest('./doc_src/__root/css'));
 });
 
-gulp.task('doc', function () {
-    return gulp.src('./docsrc/styl/doc.styl')
-        .pipe(stylus({
-            compress: true,
-            use: [autoprefixer('last 5 versions')]
-        }))
-        .pipe(gulp.dest('./documentation/css'));
-});
-
-gulp.task('md', function() {
+gulp.task('compile_doc', function() {
     return gulp.src('**/*.md')
         .pipe(gulpMdDocs({
-            templates: {
-                base: './docsrc/tmpl/base.html',
-                block: {
-                    code: './docsrc/tmpl/block/code.html',
-                    codespan: './docsrc/tmpl/block/codespan.html',
-                    hr: './docsrc/tmpl/block/hr.html',
-                    heading: './docsrc/tmpl/block/heading.html',
-                    paragraph: './docsrc/tmpl/block/paragraph.html',
-                    table: './docsrc/tmpl/block/table.html',
-                    tablerow: './docsrc/tmpl/block/tablerow.html',
-                    tablecell: './docsrc/tmpl/block/tablecell.html',
-                }
-            }
+            template: docTemplate
         }))
-        .pipe(gulp.dest('documentation'));
+        .pipe(gulp.dest('doc_dist'));
 });
 
 gulp.task('watch', function() {
-    gulp.watch('./src/styl/**/*.styl', ['desktop', 'portable']);
-    gulp.watch('./docsrc/styl/**/*.styl', ['doc']);
-    gulp.watch('**/*.md', ['md']);
+    gulp.watch('./src/styl/**/*.styl', ['compile_desktop_uikit', 'compile_portable_uikit', 'copy_template_root_dir']);
+    gulp.watch('./doc_src/styl/**/*.styl', ['compile_doc_css', 'copy_template_root_dir']);
+    gulp.watch('**/*.md', ['compile_doc']);
 });
