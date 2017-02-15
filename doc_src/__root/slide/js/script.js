168420a028676d1ceccbcfff93dae45393eb2354
e.marchenko <e.marchenko@smartweb.com.ua>
2017-02-15 08:59:24 +0200

    add nav for slider

--- doc_src/__root/slide/js/script.js
+++ doc_src/__root/slide/js/script.js
@@ -1,10 +1,10 @@
 function setActiveClass(nodeList, index) {
     [].forEach.call(nodeList, function(node, i) {
         if (i === index) {
-            document.querySelector("[data-slider-prev]").textContent = i + 1;
+            document.querySelector("[data-slider-count-prev]").textContent = i + 1;
             node.classList.add('active');
         } else {
-            document.querySelector("[data-slider-next]").textContent = nodeList.length;
+            document.querySelector("[data-slider-count-next]").textContent = nodeList.length;
             node.classList.remove('active');
         }
     });
@@ -13,22 +13,52 @@ function setActiveClass(nodeList, index) {
 
 document.addEventListener("DOMContentLoaded", function(){
     var nodeList = document.querySelectorAll("[data-slider-case]");
+    var targetNext = document.querySelector("[data-slider-next]");
+    var targetPrev = document.querySelector("[data-slider-prev]");
     var index = 0;
+
+
+    function slideToNext() {
+        if (index >= nodeList.length-1) {
+            return false;
+        }
+        setActiveClass(nodeList, ++index);
+    }
+
+    function slideToPrev() {
+        if (index <= 0) {
+            return false;
+        }
+        setActiveClass(nodeList, --index);
+    }
 
     setActiveClass(nodeList, index);
 
+    targetNext.addEventListener("click", function(e) {
+        slideToNext();
+    });
+
+    targetPrev.addEventListener("click", function(e) {
+        slideToPrev();
+    });
+
     document.onkeyup = function(e) {
         if (e.keyCode === 37) {
-            if (index <= 0) {
-                return false;
-            }
-            setActiveClass(nodeList, --index);
+            slideToPrev();
+            targetPrev.classList.remove('active');
         }
         if (e.keyCode === 39) {
-            if (index >= nodeList.length-1) {
-                return false;
-            }
-            setActiveClass(nodeList, ++index);
+            slideToNext();
+            targetNext.classList.remove('active');
+        }
+    }
+
+    document.onkeydown = function(e) {
+        if (e.keyCode === 37) {
+            targetPrev.classList.add('active');
+        }
+        if (e.keyCode === 39) {
+            targetNext.classList.add('active');
         }
     }
 });
