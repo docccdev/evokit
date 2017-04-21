91d65b9adad2a6e57d16e38ef0e40f1498644a2e
e.marchenko <e.marchenko@smartweb.com.ua>
2017-04-21 12:54:44 +0300

    fix style for doc

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var gulpMdDocs = require('gulp-md-docs');
 var docTemplate = require('./doc_src/index.js');
 
 gulp.task('compile_doc_css', function () {
-    return gulp.src('./doc_src/styl/doc.styl')
+    return gulp.src('./doc_src/styl/bundles/doc.styl')
         .pipe(stylus({
             compress: true,
             use: [autoprefixer('last 5 versions')]
@@ -39,7 +39,7 @@ gulp.task('compile_portable_uikit', function () {
 });
 
 gulp.task('compile_doc', function() {
-    return gulp.src('doc_md/**/*.md')
+    return gulp.src('./doc_md/**/*.md')
         .pipe(gulpMdDocs({
             template: docTemplate
         }))
@@ -49,5 +49,5 @@ gulp.task('compile_doc', function() {
 gulp.task('watch', function() {
     gulp.watch('./src/styl/**/*.styl', ['compile_desktop_uikit', 'compile_portable_uikit', 'copy_template_root_dir']);
     gulp.watch('./doc_src/styl/**/*.styl', ['compile_doc_css', 'copy_template_root_dir']);
-    gulp.watch('**/*.md', ['compile_doc']);
+    gulp.watch('./doc_md/**/*.md', ['compile_doc']);
 });
