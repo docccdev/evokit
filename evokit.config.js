3576598498a9eb8b0e2e2e1abb5bc0efd0cb8aa9
docccdev <doccc.dev@gmail.com>
2019-05-13 23:29:58 +0300

    change evokit.config.js

--- evokit.config.js
+++ evokit.config.js
@@ -1,4 +1,4 @@
-// EvoKit config v2.0.0
+// EvoKit config v3.0.0
 
 module.exports = {
     customProperties: {
@@ -23,90 +23,6 @@ module.exports = {
         '--ek-default-font-size': '13px',
         '--ek-default-font-weight': '400',
         '--ek-default-line-height': '1em',
-
-        // COMMON and THEME DEFAULT
-        '--ek-default-background-color': '#f7f7f7',
-        '--ek-default-border-color': '#dde8f0',
-        '--ek-default-divider-color': 'var(--ek-default-border-color)',
-        '--ek-default-text-color': '#3c3c3c',
-        '--ek-default-link-color': '#5e8fd5',
-
-        // THEME RESET
-        '--ek-reset-background-color': 'transparent',
-        '--ek-reset-border-color': 'transparent',
-        '--ek-reset-divider-color': 'transparent',
-        '--ek-reset-text-color': 'var(--ek-default-text-color)',
-        '--ek-reset-link-color': 'var(--ek-default-text-color)',
-
-        // THEME PRIMARY (brand color 1)
-        '--ek-primary-background-color': 'white',
-        '--ek-primary-border-color': 'black',
-        '--ek-primary-divider-color': 'black',
-        '--ek-primary-text-color': 'black',
-        '--ek-primary-link-color': 'black',
-
-        // THEME SECOND (brand color 2)
-        '--ek-second-background-color': 'white',
-        '--ek-second-border-color': 'black',
-        '--ek-second-divider-color': 'black',
-        '--ek-second-text-color': 'black',
-        '--ek-second-link-color': 'black',
-
-        // THEME MINOR (brand color 3)
-        '--ek-minor-background-color': 'white',
-        '--ek-minor-border-color': 'black',
-        '--ek-minor-divider-color': 'black',
-        '--ek-minor-text-color': 'black',
-        '--ek-minor-link-color': 'black',
-
-        // THEME LIGHT
-        '--ek-light-background-color': '#f8f9fa',
-        '--ek-light-border-color': '#dee2e6',
-        '--ek-light-divider-color': '#dee2e6',
-        '--ek-light-text-color': '#212529',
-        '--ek-light-link-color': '#212529',
-
-        // THEME MUTED
-        '--ek-muted-background-color': '#f7f7f7',
-        '--ek-muted-border-color': '#e5e5e5',
-        '--ek-muted-divider-color': '#e5e5e5',
-        '--ek-muted-text-color': '#a0b0c7',
-        '--ek-muted-link-color': '#a0b0c7',
-
-        // THEME DARK
-        '--ek-dark-background-color': '#343a40',
-        '--ek-dark-border-color': '#dee2e6',
-        '--ek-dark-divider-color': '#dee2e6',
-        '--ek-dark-text-color': '#ffffff',
-        '--ek-dark-link-color': '#ffffff',
-
-        // THEME SUCCESS
-        '--ek-success-border-color': '#b7e6b1',
-        '--ek-success-divider-color': '#b7e6b1',
-        '--ek-success-background-color': '#d0efcc',
-        '--ek-success-text-color': '#33b600',
-        '--ek-success-link-color': '#33b600',
-
-        // THEME INFO
-        '--ek-info-border-color': '#c1d4ed',
-        '--ek-info-divider-color': '#dde8f0',
-        '--ek-info-background-color': '#dde7f5',
-        '--ek-info-text-color': '#5e8fd5',
-        '--ek-info-link-color': '#5e8fd5',
-
-        // THEME WARNING
-        '--ek-warning-border-color': '#faea9b',
-        '--ek-warning-divider-color': '#faea9b',
-        '--ek-warning-background-color': '#fcf1bd',
-        '--ek-warning-text-color': '#ff9600',
-        '--ek-warning-link-color': '#ff9600',
-
-        // THEME DANGER
-        '--ek-danger-border-color': '#e6b9b3',
-        '--ek-danger-divider-color': '#e6b9b3',
-        '--ek-danger-background-color': '#efd2ce',
-        '--ek-danger-text-color': '#bb2914',
-        '--ek-danger-link-color': '#bb2914',
     },
     customMedia: {
         '--ek-media-small': 'only screen and (min-width: 480px)',
