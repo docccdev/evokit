2ce944376be5cdf61ff3af31f08f32d22b398395
docccdev <doccc.dev@gmail.com>
2023-12-04 08:51:57 +0200

    update bench

--- bench/index.js
+++ bench/index.js
@@ -19,22 +19,28 @@ BoxPerf.defaultProps = {
     },
 };
 
-// const PROPS = { 'box-padding': 's m l xl', 'box-margin': ['s', 'm', 'xl'], border: 'none' };
-// const PROPS = { 'box-padding': ['s', 'm', 'xl'], 'box-margin': ['s', 'm', 'xl'], border: 'none' };
+// const PROPS = { 'box-padding': 's m l xl', 'box-margin': ['s', 'm', 'xl'], 'box-border': 'none' };
+// const PROPS = { 'box-padding': ['s', 'm', 'xl'], 'box-margin': ['s', 'm', 'xl'], 'box-border': 'none' };
 const PROPS = {
     'box-padding': { s: true, m: true, xl: true },
     'box-margin': { s: true, m: true, xl: true },
-    border: 'none',
+    'box-border': 'none',
 };
 // const PROPS = { 'box-opacity': [20, 30, 40] };
 // const PROPS = { 'box-opacity': { 0: true, 20: true, 30: true, 40: true } };
 // const PROPS = { 'box-opacity': true };
-// const PROPS = { 'box-padding': 's', 'box-margin': 'm', border: 'none' };
+// const PROPS = { 'box-padding': 's', 'box-margin': 'm', 'box-border': 'none' };
 
-checkPerformance('test1', () => {
+PROPS.className = 'mix1';
+
+checkPerformance('test1 div', () => {
+    return ReactDOMServer.default.renderToString(React.createElement('div', PROPS));
+});
+
+checkPerformance('test1 Box', () => {
     return ReactDOMServer.default.renderToString(React.createElement(Box, PROPS));
 });
 
-checkPerformance('test2', () => {
+checkPerformance('test2 BoxPerf', () => {
     return ReactDOMServer.default.renderToString(React.createElement(BoxPerf, PROPS));
 });
